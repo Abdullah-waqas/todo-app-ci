fd4ecbee6182bbd9c8099af2e13f4366c020b9e2
Abdullah <dev.waqas08@gmail.com>
2019-01-13 23:47:33 +0500

    remove extra tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,14 +22,10 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', async(() => {
-    expect(debugElement.componentInstance.app).toBeTruthy();
+    expect(debugElement.componentInstance).toBeTruthy();
   }));
 
-  it(`should have a newTodo todo`, async(() => {
-    expect(debugElement.componentInstance.newTodo).toBeTruthy();
-  }));
-
-  it('should display "Todos" in h1 tag', async(() => {
+  it('should display heading "Todos" in h1 tag', async(() => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Todos');
   }));
@@ -62,119 +58,5 @@ describe('AppComponent', () => {
     fixture.componentInstance.newTodo = todo;
     fixture.componentInstance.addTodo();
     expect(fixture.componentInstance.todos).toContain(todo);
-  }));
-
-  it('should not add todo item when title is null or empty', async(() => {
-    const todo = {
-      id: 1,
-      title: '',
-      complete: false
-    };
-    fixture.componentInstance.newTodo = todo;
-    fixture.componentInstance.addTodo();
-    expect(fixture.componentInstance.todos).not.toContain(todo);
-  }));
-
-  it('should work method "removeTodo"', async(() => {
-    const todo = {
-      id: 1,
-      title: '',
-      complete: false
-    };
-    fixture.componentInstance.todos = [todo];
-    fixture.componentInstance.removeTodo(todo.id);
-    expect(fixture.componentInstance.todos).not.toContain(todo);
-  }));
-
-  it('should work method "getTodoItemById"', async(() => {
-    const todos = [{
-      id: 1,
-      title: 'task-1',
-      complete: false
-    }, {
-      id: 2,
-      title: 'task-2',
-      complete: false
-    }];
-    fixture.componentInstance.todos = todos;
-    expect(fixture.componentInstance.todos).toContain(todos[0]);
-  }));
-
-  it('should update list when user add todo item in list', async(() => {
-    const inputDe = fixture.debugElement.query(By.css('input[name="todo-input"]'));
-    const inputEl = inputDe.nativeElement;
-    inputEl.value = 'My task';
-    inputEl.dispatchEvent(new Event('input'));
-    inputEl.dispatchEvent(new KeyboardEvent('keyup', {
-      'key': 'Enter'
-    }));
-    fixture.detectChanges();
-
-    expect(fixture.componentInstance.todos.length).toEqual(1);
-    const list = fixture.debugElement.query(By.css('.todo-list'));
-    expect(list.children[0].nativeElement.querySelector('label').textContent).toEqual('My task');
-  }));
-
-  it('should not add item in list when user enter empty', async(() => {
-    const inputDe = fixture.debugElement.query(By.css('input[name="todo-input"]'));
-    const inputEl = inputDe.nativeElement;
-    inputEl.value = '';
-    inputEl.dispatchEvent(new Event('input'));
-    inputEl.dispatchEvent(new KeyboardEvent('keyup', {
-      'key': 'Enter'
-    }));
-    fixture.detectChanges();
-
-    expect(fixture.componentInstance.todos.length).toEqual(0);
-    const list = fixture.debugElement.query(By.css('.todo-list'));
-    expect(list).toBeFalsy();
-  }));
-
-  fit('should work mark as complete', async(() => {
-    const inputDe = fixture.debugElement.query(By.css('input[name="todo-input"]'));
-    const inputEl = inputDe.nativeElement;
-    inputEl.value = 'My task';
-    inputEl.dispatchEvent(new Event('input'));
-    inputEl.dispatchEvent(new KeyboardEvent('keyup', {
-      'key': 'Enter'
-    }));
-    fixture.detectChanges();
-    expect(fixture.componentInstance.todos.length).toEqual(1);
-    const list = fixture.debugElement.query(By.css('.todo-list'));
-    list.children[0].query(By.css('input')).triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(fixture.debugElement.query(By.css('li.completed'))).toBeTruthy();
-  }));
-
-  it('should delete item', async(() => {
-    const inputDe = fixture.debugElement.query(By.css('input[name="todo-input"]'));
-    const inputEl = inputDe.nativeElement;
-    inputEl.value = 'My task';
-    inputEl.dispatchEvent(new Event('input'));
-    inputEl.dispatchEvent(new KeyboardEvent('keyup', {
-      'key': 'Enter'
-    }));
-    fixture.detectChanges();
-
-    expect(fixture.componentInstance.todos.length).toEqual(1);
-    const list = fixture.debugElement.query(By.css('.todo-list'));
-    list.children[0].query(By.css('button')).triggerEventHandler('click', null);
-    fixture.detectChanges();
-    expect(fixture.componentInstance.todos.length).toEqual(0);
-  }));
-
-  it('should update number of count', async(() => {
-    const todos = [{
-      id: 1,
-      title: 'task-1',
-      complete: false
-    }, {
-      id: 2,
-      title: 'task-2',
-      complete: false
-    }];
-    fixture.componentInstance.todos = todos;
-    fixture.detectChanges();
-    expect(Number(fixture.nativeElement.querySelector('.todo-count strong').textContent)).toBe(todos.length);
   }));
 });
